ccde1ac4399d54797a196cb834f811ab66ba4e35
dale <dale@ptc.com>
2022-03-26 15:22:16 -0400

    added baseline_cnn.py

---
+++ challenge1.py
@@ -0,0 +1,62 @@
+
+from tqdm import tqdm
+import numpy as np
+import random
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+import medmnist
+from medmnist import INFO, Evaluator
+from numpy.random import RandomState
+import numpy as np
+import torch
+import torch.optim as optim
+from torch.utils.data import Subset
+import re
+from torchvision import datasets, transforms
+
+"""Now let's visualize the chest xray data"""
+
+data_flag = 'pneumoniamnist'
+info = INFO[data_flag]
+task = info['task']
+DataClass = getattr(medmnist, info['python_class'])
+
+# load the data
+train_dataset = DataClass(split='train', download=True)
+
+def train(model, device, train_loader, optimizer, epoch, display=True):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.binary_cross_entropy_with_logits(output, target.float())
+        loss.backward()
+        optimizer.step()
+    if display:
+      print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+          epoch, batch_idx * len(data), len(train_loader.dataset),
+          100. * batch_idx / len(train_loader), loss.item()))
+
+def test(model, device, test_loader, name="\nVal"):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.binary_cross_entropy_with_logits(output, target.float(), size_average=False).item() # sum up batch loss
+            pred = output >= 0.5
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+    print('{} set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        name, test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    return 100. * correct / len(test_loader.dataset)
